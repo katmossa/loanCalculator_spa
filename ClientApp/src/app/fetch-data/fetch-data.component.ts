3250534d2f38b71bc0f73451073eeb5fb0614bf1
kasia.mossakowska <kasia.mossakowska@macrix.pl>
2020-11-05 14:53:34 +0100

    Removed unused counter, add primeng

--- ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -6,18 +6,45 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[];
+  public installments: Installment[];
+  private readonly baseUrl: string;
+  private readonly loadnCalculatorCtrl: string;
+  private http: HttpClient;
+
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
-      this.forecasts = result;
+    this.baseUrl = baseUrl;
+    this.http = http;
+    this.loadnCalculatorCtrl = 'api/LoanCalculator'
+  }
+
+  public calculateInstallments() {
+    const requestUrl = `${this.baseUrl}${this.loadnCalculatorCtrl}/GenerateInstallments`;
+    const loanValues = {
+      amount: 500000,
+      paymentYears: 30,
+      PaymentStrategy: PaymentStrategy.DecreasingInstallments
+    } as Loan;
+
+    this.http.post<Installment[]>(requestUrl, loanValues ).subscribe(result => {
+      this.installments = result;
     }, error => console.error(error));
   }
 }
 
-interface WeatherForecast {
-  dateFormatted: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+interface Installment {
+  month: number;
+  principal: number;
+  interest: number;
+  installmentAmount: number;
+}
+
+interface Loan {
+  amount: number;
+  paymentYears: number;
+  PaymentStrategy: PaymentStrategy;
+}
+
+export enum PaymentStrategy {
+  DecreasingInstallments = 0
 }
